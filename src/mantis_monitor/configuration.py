4a0443aa78b04dfe4c4b38de7817cb3de374e9c6
Melanie <melanie.e.cornelius@gmail.com>
2023-03-31 15:42:39 +0000

    adding copyright and license files to everything

--- src/mantis_monitor/configuration.py
+++ src/mantis_monitor/configuration.py
@@ -1,9 +1,11 @@
 """
-Module to handle all components configuring Mantis Monitor
+This file is part of the Mantis data collection suite. Mantis, including the data collection suite (mantis-monitor) and is copyright (C) 2016 by Melanie Cornelius.
 
-Author: Melanie Cornelius
-This code is licensed under LGPL v 2.1
-See LICENSE for details
+Mantis is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+Mantis is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with Mantis. If not, see <https://www.gnu.org/licenses/>.
 """
 
 import pprint
